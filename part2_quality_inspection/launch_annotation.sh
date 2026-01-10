46ef8f703086db6e8231cc94422b0efb63c1b2d8
JiyaPandey <pandeyjiya024@gmail.com>
2026-01-10 21:17:13 +0530

    deleted part 2 files

--- part2_quality_inspection/launch_annotation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to launch LabelImg with correct configuration
-
-cd ~/ai_vision_assignment/part2_quality_inspection/data
-
-# Launch labelImg
-labelImg images/DeepPCB/ classes.txt
